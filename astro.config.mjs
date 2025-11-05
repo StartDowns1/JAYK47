efeedc66f298817c3032c23b252c7f78323eea12
jayk <92707418+StartDowns1@users.noreply.github.com>
2025-11-04 22:36:26 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  server: {
-    host: true
-  },
-  // Add this for GitHub Pages deployment
-  site: 'https://StartDowns1.github.io',
-  base: '/JAYK47',
+  output: 'static'
 });
+```
+
